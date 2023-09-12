67ebeabfef46f620b35d1157b96537198658e5cf
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2023-09-11 20:55:22 -0700

    Create subcommands
    TODO: specify discord content in subcommands (input user data)

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
+	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/state"
 	"github.com/diamondburned/arikawa/v3/utils/json/option"
 	"github.com/joho/godotenv"
 )
 
-var commands = []api.CreateCommandData{{Name: "ping", Description: "Ping!"}}
+var commands = []api.CreateCommandData{{
+	Name:        "remindme",
+	Description: "Set a reminder",
+	Options: discord.CommandOptions{
+		&discord.SubcommandOption{
+			OptionName:  "remind",
+			Description: "Set a reminder",
+		},
+		&discord.SubcommandOption{
+			OptionName:  "recur",
+			Description: "Set a recurring reminder",
+		},
+	},
+}}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -21,13 +36,46 @@ func main() {
 	}
 
 	r := cmdroute.NewRouter()
-	r.AddFunc("ping", func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+	r.AddFunc("remindme", func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
 		return &api.InteractionResponseData{Content: option.NewNullableString("Pong!")}
 	})
 
 	s := state.New("Bot " + os.Getenv("BOT_TOKEN"))
 	s.AddInteractionHandler(r)
 	s.AddIntents(gateway.IntentGuilds)
+	s.AddHandler(func(e *gateway.InteractionCreateEvent) {
+		var resp api.InteractionResponse
+
+		switch data := e.Data.(type) {
+		case *discord.CommandInteraction:
+			jsonContent, err := json.MarshalIndent(data, "", "  ")
+			if err != nil {
+				log.Println("cannot marshal data:", err)
+			}
+			log.Println(string(jsonContent))
+			resp = api.InteractionResponse{
+				Type: api.MessageInteractionWithSource,
+				Data: &api.InteractionResponseData{
+					Content: option.NewNullableString("Unknown command: " + data.Name),
+				},
+			}
+
+		case discord.ComponentInteraction:
+			resp = api.InteractionResponse{
+				Type: api.UpdateMessage,
+				Data: &api.InteractionResponseData{
+					Content: option.NewNullableString("Custom ID: " + string(data.ID())),
+				},
+			}
+		default:
+			log.Printf("unknown interaction type %T", e.Data)
+			return
+		}
+
+		if err := s.RespondInteraction(e.ID, e.Token, resp); err != nil {
+			log.Println("failed to send interaction callback:", err)
+		}
+	})
 
 	if err := cmdroute.OverwriteCommands(s, commands); err != nil {
 		log.Fatalln("cannot update commands:", err)
---
+++ remindme.go
@@ -0,0 +1,11 @@
+package main
+
+// var remindme struct {
+// 	Title       string         `json:“title”`
+// 	Date        string         `json:“date”`
+// 	Time        string         `json:“timestamp”`
+// 	Location    string         `json:“location?”`
+// 	Details     string         `json:“details?”`
+// 	Remindme    string         `json:“timestamp”`
+// 	AuthorID    discord.UserID `json:“author_id”`
+// }
